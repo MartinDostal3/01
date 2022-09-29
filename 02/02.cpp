acb9d47f1a00497c34aadcca4d6cf2ec900b44cf
dostalm.04 <dostalm.04@spst.eu>
2022-09-29 11:20:08 +0200

    02,03

--- 02/02.cpp
+++ 02/02.cpp
@@ -8,7 +8,84 @@ int main()
 {
     
 
+    srand((unsigned)time(NULL));
+    char volba;
+    cout << "\nF = For, W = While, D = DO While, K=Konec: ";
+    cin >> volba;
+    while (volba != 'K')
+    {
+        switch (volba)
+        {
+        case 'F':
+        {
+            int n;
+            double soucet = 0;
+            cout << "Zadej cislo n: ";
+            cin >> n;
 
+            for (int i = 0; i < n; i++)
+            {
+                double nc = 10 + (100 - 10) * (double)rand() / (RAND_MAX + 1);
+                soucet += nc;
+                cout << nc << endl;
+            }
+            cout << "Soucet je " << soucet << endl;
+        }
+        break;
+        case 'W':
+        {
+            int n;
+            int i = 0;
+            double soucet = 0;
+            cout << "Zadej cislo n: ";
+            cin >> n;
+
+            while (i < n)
+            {
+                double nc = 10 + (100 - 10) * (double)rand() / (RAND_MAX + 1);
+                soucet += nc;
+                cout << nc << endl;
+
+                ++i;
+            }
+            cout << "Soucet je " << soucet << endl;
+        }
+        case 'D':
+        {
+            int n;
+            int i = 0;
+            double soucet = 0;
+            cout << "Zadej cislo n: ";
+            cin >> n;
+
+            do {
+                double nc = 10 + (100 - 10) * (double)rand() / (RAND_MAX + 1);
+                soucet += nc;
+                cout << nc << endl;
+                if (i == 0)
+                {
+                  
+                    soucet = 0;
+                }
+                else
+                {
+                    cout << nc;
+                }
+
+                ++i;
+            } while (i < n);
+            cout << "Soucet je " << soucet << endl;
+        }
+        break;
+        default:
+        {
+            cout << "Nevybrana moznost";
+        }
+        break;
+        }
+        cout << "\n F = For, W = While, D = DO While, K=Konec: ";
+        cin >> volba;
+    }
 }
 
 
